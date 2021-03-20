8f2d30ab6e4ef3cf780a733c3d26ba3c0ebcb8d3
Atharva Ikhar <atharva464@gmail.com>
2021-03-20 22:59:27 +0530

    splash screen patched with lottie animation

--- app/src/main/java/com/example/bookcave/MainActivity.java
+++ app/src/main/java/com/example/bookcave/MainActivity.java
@@ -7,22 +7,20 @@ import android.os.Bundle;
 import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
-    private static int SPLASH_TIME_OUT=1500;
+    private static int SPLASH_TIME_OUT=1700;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
         //Go to Log in
-        /*
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent i = new Intent(MainActivity.this,.class);
+                Intent i = new Intent(MainActivity.this,CustomerLogin.class);
                 startActivity(i);
                 finish();
             }
         },SPLASH_TIME_OUT);
-         */
     }
 }

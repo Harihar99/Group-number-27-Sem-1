033a471c8e3a8aa1cf91d2f1180c64deeb283e04
Atharva Ikhar <atharva464@gmail.com>
2021-09-23 22:32:06 +0530

    Added ids for buy book components

--- app/src/main/java/com/example/bookcave/BuyBook.java
+++ app/src/main/java/com/example/bookcave/BuyBook.java
@@ -1,14 +1,24 @@
 package com.example.bookcave;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class BuyBook extends AppCompatActivity {
 
+    public TextView oname,onumber,oaddress,obname,obauthorname,ogenre,orpice,oitemprice,odeliprice,ototalprice,famount;
+    public Button changeaddress,order;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buy_book);
+
+        oname=findViewById(R.id.oname);
+        onumber=findViewById(R.id.onumber);
+        oaddress=findViewById(R.id.oaddress);
+        obname=findViewById(R.id.obname);
+        obauthorname=findViewById(R.id.obauthorname);
     }
 }
